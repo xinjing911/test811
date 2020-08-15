4327380fe0e3b1d57d65461f7fcf0b5bcb350dc6
xinjing <xinjing77@yeah.net>
2020-08-15 10:28:42 +0800

    2

--- st/test_cases/test_departments/test_depQuery_all.py
+++ st/test_cases/test_departments/test_depQuery_all.py
@@ -1,4 +1,4 @@
-# 导包
+﻿# 导包
 import unittest
 import  requests
 
@@ -6,12 +6,12 @@ import  requests
 class TestDepQueryAll(unittest.TestCase):
     # 查询学院所有冒烟测试
     def test_depQuery_all(self):
-        url= r'http://127.0.0.1:8000/api/departments/'
+        url= r'http://10.10.63.187:8000/api/departments/'
         res = requests.get(url)
         self.assertEqual(200,res.status_code)
     # 查询学院所有，用错误的方法
     def test_depQuery_all_wrongMethod(self):
-        url= r'http://127.0.0.1:8000/api/departments/'
+        url= r'http://10.10.63.187:8000/api/departments/'
         res = requests.post(url)
         self.assertEqual(200,res.status_code)
 if __name__ == '__main__':
