a8c6eeb13aa406e4e153b469ef60112e550ba5da
xinjing <xinjing77@yeah.net>
2020-08-16 12:33:55 +0800

    9

--- st/test_cases/test_departments/test_depQuery_byName.py
+++ st/test_cases/test_departments/test_depQuery_byName.py
@@ -5,12 +5,12 @@ import  requests
 # 设置测试类
 class TestDepQueryByName(unittest.TestCase):
     def test_depQuery_byName(self):
-        url = r'http://10.10.63.187:8000/api/departments/'
+        url = r'http://127.0.0.1:8000/api/departments/'
         params = {"dep_name":"Test学院"}
         res = requests.get(url,params)
         self.assertEqual(200,res.status_code)
     def  test_depQuery_byName_notExistsName(self):
-        url = r'http://10.10.63.187:8000/api/departments/'
+        url = r'http://127.0.0.1:8000/api/departments/'
         params = {"dep_name": "dfasfasf"}
         res = requests.get(url, params)
         self.assertEqual(200, res.status_code)
